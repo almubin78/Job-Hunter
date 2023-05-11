dab71de523c21224b22778f7a276ed6ec4f5fa7c
Al Mubin <almubin78@gmail.com>
2023-05-11 10:38:29 +0600

    'simple-setup-completed-now-focus-on-Home-Page-design'

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -3,15 +3,19 @@ import { NavLink } from 'react-router-dom';
 import './navigation.css'
 
 const Navigation = () => {
+    const linkStyle ={
+        marginLeft:'20px',
+
+    }
     return (
         <div>
             <div className="logo"></div>
             <div className="linkSection">
-                <NavLink style={(isActive)=>{return {color:isActive?'':''}}} to='/'>Home</NavLink>
-                <NavLink to='/'>Statistic</NavLink>
-                <NavLink to='/details'>Details</NavLink>
-                <NavLink to='/'>Applied Jobs</NavLink>
-                <NavLink to='/'>Home</NavLink>
+                <NavLink style={(isActive)=>{return {color:isActive ?' ':''}}} to='/'>Home</NavLink>
+                {/* <NavLink style={(isActive)=>{return {color:isActive ?' ':''}}} to='/'>Home</NavLink> */}
+                <NavLink style={linkStyle} to='/statistics'>Statistic</NavLink>
+                <NavLink style={linkStyle} to='/details'>Details</NavLink>
+                <NavLink style={linkStyle} to='/d'>Applied Jobs</NavLink>
             </div>
             <div className="btn"></div>
         </div>
--- src/component/router/router.js
+++ src/component/router/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
 import Home from "../Home/home/Home";
 import JobDetails from "../JobDetails/JobDetails";
+import Statistics from "../Stastics/Statistics";
 
 const router = createBrowserRouter([
     {
@@ -9,12 +10,16 @@ const router = createBrowserRouter([
         element:<Main/>,
         children:[
             {
-                path:'/',
+                path:'/d',
                 element:<Home></Home>
             },
             {
                 path:'/details',
                 element:<JobDetails></JobDetails>
+            },
+            {
+                path:'/statistics',
+                element:<Statistics></Statistics>
             }
         ]
     }
