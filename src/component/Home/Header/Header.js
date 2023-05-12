1ae5b5c28ceab373a14ad42086332017bfa9343e
Al Mubin <almubin78@gmail.com>
2023-05-12 19:51:51 +0600

    'header-section-design-completed'

--- src/component/Home/Header/Header.js
+++ src/component/Home/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import rightLogo from '../../../assets/All Images/P3OLGJ1 copy 1.png'
 
 const Header = () => {
     return (
-        <div>
-            h eader
+        <div className=' md:flex items-center mt-3 mb-5 container'>
+            <div className="content-section ">
+                <div>
+                    <h2 className='text-xl'>One Step Closer To Your  </h2>
+                    <h2 className='text-purple-500 text-2xl'>Dream Job</h2>
+                    <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                </div>
+                <button className='btn'>Get Started</button>
+            </div>
+            <div className="img-section ">
+                <img src={rightLogo} alt="" />
+            </div>
         </div>
     );
 };
