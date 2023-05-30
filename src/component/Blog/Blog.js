86acab957027c2202a33f7996312d1cfe9b1668b
Al Mubin <almubin78@gmail.com>
2023-05-30 11:24:32 +0600

    blog section details

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Blog = () => {
+    const styles = {
+        margin:"100px",
+        boxShadow:"5px 3px 7px green",
+        padding:"5%"
+    }
+
     return (
-        <div>
+        <div style={styles}>
             <h3>এই সেকশন করতে এসে দেখি মার্কস পেয়ে গেছি। </h3>
-            <h5>এখন এটার রিপো চেঞ্জ করে নিতে হবে।</h5>
+            <h5>এখন এটার রিপো চেঞ্জ করে নিয়েছি পাবলিকে।</h5>
         </div>
     );
 };
